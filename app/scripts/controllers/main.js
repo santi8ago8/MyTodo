003f8ba4e57fd21a0cf1bb23712428725a913cc0
santi8ago8 <santi8ago8@gmail.com>
2014-11-11 12:16:19 -0300

    add remove from server

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,32 +8,37 @@
  * Controller of the mitodoApp
  */
 angular.module('mitodoApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-
+  .controller('MainCtrl', function ($scope, $http) {
     $scope.search = '';
 
-    $scope.todos = [
-      {text:"comprar zapatillas"},
-      {text:"comprar libreta"},
-      {text:"comprar comida"},
-      {text:"aaaa primera"},
-      {text:"comer comida"},
-      {text:"sacar el perro"}
-    ];
+    $http.get('/api/list')
+      .success(function (data, status, header, config) {
+        $scope.todos = data.data;
+        console.log(data);
+      });
 
-    $scope.addTodo = function(){
-      if ($scope.todo){
-        $scope.todos.push({text:$scope.todo});
-        $scope.todo = '';
+    $scope.addTodo = function () {
+      if ($scope.todo) {
+
+        $http.post('/api/add', {
+          todo: $scope.todo
+        })
+          .success(function (data, status, header, config) {
+            console.log(data);
+            $scope.todos.push($scope.todo);
+            $scope.todo = '';
+          });
       }
     };
 
-    $scope.removeTodo=function(index){
-      $scope.todos.splice(index,1);
+    $scope.removeTodo = function (index) {
+      $http.post('/api/remove', {
+        todo: $scope.todos[index]
+      })
+        .success(function (data, status, header, config) {
+          console.log(data);
+          $scope.todos.splice(index, 1);
+        });
+
     };
   });
